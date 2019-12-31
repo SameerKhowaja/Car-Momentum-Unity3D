165a69c707d734e70ae8363d0539cdea330e3e29
Sameer Khowaja <sameerkhowaja12@gmail.com>
2019-12-31 19:40:20 +0500

    Car move only

---
+++ Car Movementum/Assets/Script/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform objectFollow;
+    public Vector3 offset;
+    public float followSpeed = 10;
+    public float lookSpeed = 10;
+
+    public void lookAtTarget()
+    {
+        Vector3 lookDirection = objectFollow.position - transform.position;
+        Quaternion rot = Quaternion.LookRotation(lookDirection, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, lookSpeed * Time.deltaTime);
+    }
+
+    public void moveToTarget()
+    {
+        Vector3 targetPos = objectFollow.position + objectFollow.forward * offset.z +
+                            objectFollow.right * offset.x + objectFollow.up * offset.y;
+        transform.position = Vector3.Lerp(transform.position, targetPos, followSpeed * Time.deltaTime);
+    }
+
+    public void FixedUpdate()
+    {
+        lookAtTarget();
+        moveToTarget();
+    }
+
+}
---
+++ Car Movementum/Assets/Script/CarController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private float m_horizontalInput, m_verticleInput, m_steerAngle;
+    public WheelCollider wheel1, wheel2, wheel3, wheel4;
+    public Transform wheel1T, wheel2T, wheel3T, wheel4T;
+    public float maxSteerAngle = 30;
+    public float motorForce = 50;
+
+    public void GetInput()
+    {
+        m_horizontalInput = Input.GetAxis("Horizontal");
+        m_verticleInput = Input.GetAxis("Vertical");
+    }
+    private void Steer()
+    {
+        m_steerAngle = maxSteerAngle * m_horizontalInput;
+        wheel1.steerAngle = m_steerAngle;
+        wheel2.steerAngle = m_steerAngle;
+    }
+    private void Acceleration()
+    {
+        wheel1.motorTorque = m_verticleInput * motorForce;
+        wheel2.motorTorque = m_verticleInput * motorForce;
+    }
+    private void UpdateWheelPosses()
+    {
+        UpdateWheelPose(wheel1, wheel1T);
+        UpdateWheelPose(wheel2, wheel2T);
+        UpdateWheelPose(wheel3, wheel3T);
+        UpdateWheelPose(wheel4, wheel4T);
+    }
+    private void UpdateWheelPose(WheelCollider collider, Transform transform)
+    {
+        Vector3 pos = transform.position;
+        Quaternion quant = transform.rotation;
+
+        collider.GetWorldPose(out pos, out quant);
+        transform.position = pos;
+        transform.rotation = quant;
+    }
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Acceleration();
+        UpdateWheelPosses();
+    }
+}
